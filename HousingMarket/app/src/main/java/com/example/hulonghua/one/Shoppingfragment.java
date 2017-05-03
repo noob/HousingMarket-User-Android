f67997d02ebc1efd9b2634590b1d5d106e339eb8
Hutao1 <860366929@qq.com>
2017-05-03 21:35:28 +0800

    only time make four fragment

---
+++ HousingMarket/app/src/main/java/com/example/hulonghua/one/Shoppingfragment.java
@@ -0,0 +1,9 @@
+package com.example.hulonghua.one;
+
+import android.app.Fragment;
+
+/**
+ * Created by hulonghua on 2017/5/3.
+ */
+public class Shoppingfragment extends Fragment {
+}
---
+++ HousingMarket/app/src/main/java/com/example/hulonghua/one/TrueMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hulonghua.one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrueMainActivity extends AppCompatActivity {
+    private Shoppingfragment shoppingfragment;
+    private Minefragment minefragment;
+    private Indentfragment indentfragment;
+    private Homepagefragment homepagefragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_true_main);
+    }
+}
